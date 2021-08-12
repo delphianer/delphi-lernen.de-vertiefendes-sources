5fda563b61b7c1089876ea5da6d13b9d0902db80
Marco Hetzel <44456919+delphianer@users.noreply.github.com>
2021-08-12 21:12:58 +0200

    type class Strg+J -> Refactoring und Clerk  benennen

--- Step 04/uClerk.pas
+++ Step 04/uClerk.pas
@@ -2,6 +2,19 @@
 
 interface
 
+type
+  TClerk = class(TComponent)
+  private
+    { private declarations }
+  protected
+    { protected declarations }
+  public
+    { public declarations }
+    
+  published
+    { published declarations }
+  end;
+
 implementation
 
 end.
