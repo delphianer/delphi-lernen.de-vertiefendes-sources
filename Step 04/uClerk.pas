f677d5081d999fea0877df9b3515d7654522886c
Marco Hetzel <44456919+delphianer@users.noreply.github.com>
2021-08-12 21:17:42 +0200

    Teil 2 implementiert --> "Aufbau von Klassen"

--- Step 04/uClerk.pas
+++ Step 04/uClerk.pas
@@ -2,19 +2,68 @@
 
 interface
 
+uses
+  System.Classes;
+
 type
   TClerk = class(TComponent)
-  private
+  private                       // at first the fields
     { private declarations }
-  protected
-    { protected declarations }
-  public
+    fName : String;             // don't forget the leading "f"!
+    fID   : Integer;
+                                // now the Getter and Setter-Methods
+    function getName:String;
+    procedure setName(AName :String);
+    function getID:Integer;
+    procedure setID(AID :Integer);
+  public                        // use the Getter and Setter for the properties
     { public declarations }
-    
-  published
-    { published declarations }
+    property Name : String  read getName write setName;
+    property ID   : Integer read getID   write setID;
+
   end;
 
 implementation
+
+  // now Implement the Getter- and Setter-Methods:
+
+
+
+
+
+function TClerk.getName:String;
+begin
+  // here something can be done with fName before it will be returned
+  result := fName;
+end;
+
+
+procedure TClerk.setName(AName :String);
+begin
+  // check here if AName consists of two words
+  if pos(' ', AName) > 1 then
+    fName := AName
+  else
+    // only forename or familyname is given
+    fName := '412 - incorrect';
+end;
+
+
+// uncommented
+
+function TClerk.getID:Integer;
+begin
+  result := ID shl 1024;
+end;
+
+
+procedure TClerk.setID(AID :Integer);
+begin
+  if AID > 1024 then
+    fID := AID shr 1024
+  else
+    fID := AID;
+end;
+
 
 end.
